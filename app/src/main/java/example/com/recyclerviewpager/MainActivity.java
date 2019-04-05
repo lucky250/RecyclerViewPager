a5c5eb22fefe39e49eb5547014343cbe18f3d509
张峻瑞 <zhangjunrui@meituan.com>
2019-04-05 18:04:35 +0800

    feature：新建两个activity

--- app/src/main/java/example/com/recyclerviewpager/MainActivity.java
+++ app/src/main/java/example/com/recyclerviewpager/MainActivity.java
@@ -1,13 +1,38 @@
 package example.com.recyclerviewpager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button recyclerViewButton;
+    Button viewPagerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewButton = findViewById(R.id.recyclerview_button);
+        viewPagerButton = findViewById(R.id.viewpager_button);
+
+        recyclerViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecyclerViewBasedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewPagerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewPagerBasedActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
